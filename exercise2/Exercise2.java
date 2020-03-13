5f5fe0ec29c92027ba67c36eba68c23667f454d5
shamilshahimi2000 <61263165+shamilshahimi2000@users.noreply.github.com>
2020-03-13 15:53:38 +0800

    Add files via upload

---
+++ exercise2/Exercise2.java
@@ -0,0 +1,35 @@
+
+package exercise2;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        Income income;
+        Staff staff;
+        String name,id,incomeType;
+        double tax,amount;
+        
+        System.out.print("name\t\t: ");
+        name=sc.nextLine();
+        System.out.print("id\t\t: ");
+        id=sc.nextLine();
+        System.out.print("income type\t: ");
+        incomeType = sc.nextLine();
+        System.out.print("income amount\t: RM");
+        amount = sc.nextDouble();
+        System.out.print("tax percentage\t: ");
+        tax = sc.nextDouble();
+        
+        
+        income=new Income(incomeType,amount,tax);
+        staff=new Staff(name,id,income);
+        System.out.print("\n---------income info-----------");
+        staff.displayIncomeinfo();
+        System.out.print("----------Staff info-----------");
+        staff.displayStaffInfo();
+    }
+}
